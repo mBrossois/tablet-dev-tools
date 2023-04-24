e46a8ac1b58a0d5c1dc50fc490dd98cf876f2a53
Mark  de Neut Brossois <mark.dnb@live.nl>
2023-04-24 19:46:26 +0000

    added button to toggle message screen

--- tablet-dev-tools/src/runtime/components/tablet-dev-tools-main.vue
+++ tablet-dev-tools/src/runtime/components/tablet-dev-tools-main.vue
@@ -1,15 +1,19 @@
 <template>
 <div class="log-component">
-    <div class="log-container">
-        <div v-for="log, index in logs" :key="index" class="log-screen">
-            <p v-for="messsage, messageIndex in log.message" :key="messageIndex" :class="log.type"><strong>Log:</strong> {{log.message[messageIndex]}}</p>
+    <div :class="{open: isOpen}" class="log-inner-component">
+        <div class="log-container">
+            <div v-for="log, index in logs" :key="index" class="log-screen">
+                <p v-for="messsage, messageIndex in log.message" :key="messageIndex" :class="log.type"><strong>Log:</strong> {{log.message[messageIndex]}}</p>
+            </div>
         </div>
     </div>
+    <button @click="toggle">C</button>
 </div>
 </template>
 
 <script setup lang="ts">
 import {ref, onMounted, onBeforeUnmount, watch} from 'vue'
+ const isOpen = ref(false)
  const logs = ref([{}])
  const originalConsoleLog = console.log
  const originalConsoleWarn = console.warn
@@ -29,16 +33,27 @@ console.error = function() {
     logs.value.push({type: "error", message: [...arguments]})
     originalConsoleError.apply(console, arguments)
 }
+
+function toggle() {
+    isOpen.value = !isOpen.value
+}
  
 </script>
 
 <style scoped>
-.log-component {
-    width: 25rem;
-    height: 100vh;
+.log-component{
     position: absolute;
-    top: 0;
     right: 0;
+    bottom: 5rem;
+
+}
+.log-inner-component {
+    transition: width 200ms ease-in-out;
+    width: 0rem;
+    height: 25rem;
+}
+.log-inner-component.open {
+    width: 25rem;
 }
 .log-container{
     height: 100%;
